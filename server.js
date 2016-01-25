47db095933da19ec7b133c393fe9885c3bd317ac
karunakar <akreddy888@gmail.com>
2016-01-25 16:21:11 +0530

    Updated

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var PORT = 3001;
 
 var middleware = {
 	requireAuthentication: function (req, res, next) {
-		console.log('private route hit');
+		console.log('private route hit!');
 		next();
 	},
 	logger: function (req, res, next) {
